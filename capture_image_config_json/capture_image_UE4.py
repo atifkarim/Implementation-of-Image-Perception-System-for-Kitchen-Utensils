d9c6e9af88067b8135d5ca100b13d9238dc6abba
atifkarim <atif0902082@gmail.com>
2019-06-29 03:02:53 +0200

    remove unnecessary lines and added comment

--- capture_image_config_json/capture_image_UE4.py
+++ capture_image_config_json/capture_image_UE4.py
@@ -1,3 +1,15 @@
+# check	
+# https://stackoverflow.com/questions/36098241/using-findcontours-in-python-with-opencv	
+# https://stackoverflow.com/questions/36160638/opencv-contours-in-python-how-to-solve-list-index-out-of-range?noredirect=1&lq=1	
+#https://stackoverflow.com/questions/31475634/drawcontours-not-working-opencv-python
+
+# =============================================================================
+#  Observing spherical movement of the camera around the object
+#  the area cover with polar_angle/elevation angle is 'Latitude' region. From north to South or vice versa
+#  the area cover with azimuthal angle is 'Longitude' region. From west to east or vice versa
+# =============================================================================
+
+
 import time; print(time.strftime("The last update of this file: %Y-%m-%d %H:%M:%S", time.gmtime()))
 import sys, time
 # Establish connection with the UE4 game
@@ -23,9 +35,9 @@ import cv2
 import copy
 crop=0
 
-#hide_Floor=client.request('vset /object/Floor/hide')
-#hide_Floor_14=client.request('vset /object/Floor_14/hide')
-
+# =============================================================================
+# making of annotation text file
+# =============================================================================
 def do_annotation(path_of_text_file,split_lit_image_name_0,x_1,y_1,x_2,y_2,class_number):
     
     f = open(path_of_text_file+str(split_lit_image_name_0)+'.txt', 'w')
@@ -38,12 +50,11 @@ def do_annotation(path_of_text_file,split_lit_image_name_0,x_1,y_1,x_2,y_2,class
 
     print('Pre Annotation File has made !!')
     
-    
-
-
-
+# =============================================================================
+# cropping function   
+# =============================================================================
 def do_crop(path_of_RGB,path_of_MASK,path_of_CROP,lit_image_name,mask_image_name,crop_image_type,class_number,path_of_TEXT,red,green,blue):
-#    print("function_start!!!!!!!!!!!!")
+
     global crop
     split_lit_image_name=lit_image_name.split(".")
     split_lit_image_name_0 = split_lit_image_name[0]
@@ -66,19 +77,6 @@ def do_crop(path_of_RGB,path_of_MASK,path_of_CROP,lit_image_name,mask_image_name
     ret,thresh = cv2.threshold(imgray,127,255,0)
     image, contours, hierarchy =  cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    
-   # image_mask_copy = read_mask_image.copy()
-#    imgray=cv2.cvtColor(read_mask_image,cv2.COLOR_BGR2GRAY)
-#    ret,thresh = cv2.threshold(imgray,127,255,1)
-#    print('Till now OK')
-#    image, contours, hierarchy =  cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.drawContours(image_mask_copy,contours,0,(0,255,0))
-    
-#    hsv = cv2.cvtColor(read_mask_image, cv2.COLOR_BGR2HSV)
-#    hsv_channels = cv2.split(hsv)
-#    _,thresh=cv2.threshold(hsv_channels[1],140,255,cv2.THRESH_BINARY_INV)
-#    im2,contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#    cv2.drawContours(read_mask_image, contours, 0, (0,255,0), 3)
     if len(contours)>0:
         print('length is: ',len(contours))
         cnt = contours[0]
@@ -101,7 +99,9 @@ def do_crop(path_of_RGB,path_of_MASK,path_of_CROP,lit_image_name,mask_image_name
         pass
 
 
-#config file is opening and fetching data
+# =============================================================================
+# config file is opening and fetching data
+# =============================================================================
 with open('test_config.json', 'r') as f:
     config = json.load(f)
 
@@ -135,6 +135,10 @@ for i in config['actor']:
     actor_dict[i].append(config['actor'][i]['radius'])
 print('actor_dict: ',actor_dict)
 
+
+# =============================================================================
+# finding index of actor_dict
+# =============================================================================
 ind= {k_1:i_1 for i_1,k_1 in enumerate(actor_dict.keys())}
 
 for u_1 in actor_dict:
@@ -144,11 +148,11 @@ for u_1 in actor_dict:
 #res_list=client.request('vget /objects')
 #print('res_type: ',type(res_list),'res_list: ',res_list)
 
-# Observing spherical movement of the camera around the object
-# the area cover with polar_angle/elevation angle is 'Latitude' region. From north to South or vice versa
-# the area cover with azimuthal angle is 'Longitude' region. From west to east or vice versa
+
+# =============================================================================
+# creating text file to contatin RGB info of actor
+# =============================================================================
 store=[]
-print('my type is: ', type(store))
    
 for i in actor_dict:
     RGB_info = 'RGB_info'
@@ -165,7 +169,7 @@ for i in actor_dict:
     f = open(str(dirName_RGB_info)+'color_info_'+str(i)+'.txt', 'r+')
     f.truncate(0)
     get_mask_color= client.request('vget /object/'+str(i)+'/color')
-    print('mask color: ',get_mask_color)
+#    print('mask color: ',get_mask_color)
     ww = str(get_mask_color)
     ww=ww.replace('R','')
     ww=ww.replace('G','')
@@ -178,103 +182,54 @@ for i in actor_dict:
     f.write(ww)
     f.write("\n")
     f.close()
-    
+
+
+# =============================================================================
+# opening the text file containing RGB info and store the r,g,b value in an array   
+# =============================================================================
 for i in actor_dict:   
     with open(str(dirName_RGB_info)+'color_info_'+str(i)+'.txt') as f1:
         all_lines = f1.readlines()
         for line in all_lines:
             line_split = line.split('\n')
             m_1 = line_split[0].split(' ')
-            print('DONE')
             store.append(m_1)
 store=np.array(store,dtype=int)
 
 r,g,b,a=[store[:,d] for d in range(len(store[0]))]
-print(r,'\t',g,'\t',b,'\t',a)
-print(r.shape,'\t',g.shape,'\t',b.shape,'\t',a.shape)
+#print(r,'\t',g,'\t',b,'\t',a)
+#print(r.shape,'\t',g.shape,'\t',b.shape,'\t',a.shape)
+
 for i in actor_dict:
-#    print('i is: ',i)
     hide=client.request('vset /object/'+str(i)+'/hide')
 #    print('hidden actor: ',i,'\t',hide)
-#number_1 = 0
+
+
 for i in actor_dict:
     r_1 = r[ind[i]]
     g_1 = g[ind[i]]
     b_1 = b[ind[i]]
     
-#    number_1 +=1
-    print(r_1,'\t',g_1,'\t',b_1)
+#    print(r_1,'\t',g_1,'\t',b_1)
     object_class=actor_dict[i][-2]
     
     print('Working with ',i,' whose class is ',object_class,' has started')
     show=client.request('vset /object/'+str(i)+'/show')
-    
-#    RGB_info = 'RGB_info'
-#    sub_dir = str(RGB_info)
-#    dirName_RGB_info = 'F:/unreal_cv_documentation/my_dir/'+sub_dir+'/'
-#    
-#    if not os.path.exists(dirName_RGB_info):
-#        
-#        os.mkdir(dirName_RGB_info)
-#        print('Directory ',dirName_RGB_info,' created')
-#    else:
-#        pass
-#    f = open(str(dirName_RGB_info)+'color_info_'+str(i)+'.txt', 'a')
-#    f = open(str(dirName_RGB_info)+'color_info_'+str(i)+'.txt', 'r+')
-#    f.truncate(0)
-#    get_mask_color= client.request('vget /object/'+str(i)+'/color')
-#    print(get_mask_color)
-#    ww = str(get_mask_color)
-#    ww=ww.replace('R','')
-#    ww=ww.replace('G','')
-#    ww=ww.replace('B','')
-#    ww=ww.replace('A','')
-#    ww=ww.replace('=','')
-#    ww=ww.replace('(','')
-#    ww=ww.replace(')','')
-#    ww=ww.replace(',',' ')
-#    f.write(ww)
-#    f.write("\n")
-#    f.close()
-#    
-#    store=[]
-#    with open(str(dirName_RGB_info)+'color_info_'+str(i)+'.txt') as f1:
-#        a=f1.readlines()
-#        for c in a:
-#            k=c.split('\n')
-#            m=k[0].split(' ')
-#            store.append(m)
-#    store=np.array(store,dtype=int)
-#
-#    r,g,b,a=[store[:,d] for d in range(len(store[0]))]
-#    print(r,'\t',g,'\t',b,'\t',a)
-    
-#    set_mask_color= client.request('vset /object/'+str(i)+'/color 255 0 0')
-#    print('set mask color: ',set_mask_color)
-#    print('visible actor: ',i,'\t',show)
-    
+
 #    print("\nJOB_START")
     pic_num=1
     Labels='Labels'
-#    num_1='00'
-#    print(str(num_1))
-#    text_num=1
     
-#    creating directory to save present actor's captured image. here at firts create the folder written inside the inverted comma
-#    for example here it is  F:/unreal_cv_documentation/my_dir/
+# =============================================================================
+#     creating directory to save present actor's captured image. here at firts create the folder written inside the inverted comma
+#     for example here it is  F:/unreal_cv_documentation/my_dir/
+# =============================================================================
     
 #    dirName=r'F:/unreal_cv_documentation/my_dir/'+str(num_1)+str(object_class)+'/'+str(i)+'/'
     dirName_RGB='F:/unreal_cv_documentation/my_dir/'+str(viewmode_1)+'/'+'00'+str(object_class)+'/'
     dirName_MASK='F:/unreal_cv_documentation/my_dir/'+str(viewmode_2)+'/'+'00'+str(object_class)+'/'
     dirName_CROP='F:/unreal_cv_documentation/my_dir/'+str(viewmode_crop)+'/'+'00'+str(object_class)+'/'
     dirName_TEXT_FILE='F:/unreal_cv_documentation/my_dir/'+str(Labels)+'/'+'00'+str(object_class)+'/'
-#    my_dir= 'F:/unreal_cv_documentation/my_dir/'+str(i)
-#    print(dirName)
-    
-#    os.makedirs(dirName_RGB)
-#    os.makedirs(dirName_MASK)
-#    os.makedirs(dirName_CROP)
-#    os.makedirs(dirName_TEXT_FILE)
 
     if not os.path.exists(dirName_RGB and dirName_MASK and dirName_CROP and dirName_TEXT_FILE):
         os.makedirs(dirName_RGB)
@@ -286,14 +241,19 @@ for i in actor_dict:
         print("Directory " , dirName_CROP ,  " Created ")
         print("Directory " , dirName_TEXT_FILE ,  " Created ")
     else:
-        pass
-#        shutil.rmtree(my_dir,ignore_errors=True)
-##        print('deleted old folder')
-#        os.mkdir(dirName)
-#        print('created new folder')
-#        print("Directory " , dirName ,  " already exists")
+        shutil.rmtree(dirName_RGB,ignore_errors= True)
+        shutil.rmtree(dirName_MASK,ignore_errors= True)
+        shutil.rmtree(dirName_CROP,ignore_errors= True)
+        shutil.rmtree(dirName_TEXT_FILE,ignore_errors= True)
+        print('deleted old folder')
+        os.makedirs(dirName_RGB)
+        os.makedirs(dirName_MASK)
+        os.makedirs(dirName_CROP)
+        os.makedirs(dirName_TEXT_FILE)
         
-#    getting the present actor's location
+# =============================================================================
+#     getting the present actor's location
+# =============================================================================
     
     actor_location=client.request('vget /object/'+str(i)+'/location')
     print(actor_location)
@@ -302,10 +262,14 @@ for i in actor_dict:
     print(actor_location_array)
     actor_location_array = actor_location_array.astype(np.float) # make the string type to float type to use in the calculation
 
-#    calculation process starts from here
+# =============================================================================
+#     calculation process starts from here
+# =============================================================================
     for polar_angle in range(polar_angle_start,polar_angle_end,polar_angle_step):
     
-#        calculating the pitch value for different polar angle
+# =============================================================================
+#         calculating the pitch value for different polar angle
+# =============================================================================
         pitch=(180-(90+polar_angle))*(-1) #rotation around the y axis(you can denote by alpha)
     
         yaw=180 #rotaion around the z-axis(you can denote by 'beta')
@@ -344,16 +308,6 @@ for i in actor_dict:
 #             if you want to use address info from config file then please use the following line
 #            res = client.request('vget /camera/0/'+str(camera_view_type)+str(" ")+str(address)+str(pic_num)+'.'+str(image_type)+'')
         
-# =============================================================================
-#             old code , you can ignore it
-#            res = client.request('vget /camera/0/viewmode_1 address'+str(pic_num)+'.png')
-#             res = client.request('vget /camera/0/lit F:/save_image_ai/object_subtraction_for_UE4/image_AI/rgb_table_4_21/'+str(pic_num)+'.png')
-# =============================================================================
-            #hide=client.request('vset /object/'+str(i)+'/hide')
-            #res_lit_only_env = client.request('vget /camera/0/'+str(viewmode_1)+str(" ")+str(dirName)+str(pic_num)+"_env"+'.'+str(image_type))
-#            print('name here: ',res_lit_only_env)
-            #show=client.request('vset /object/'+str(i)+'/show')
-#            print("here dirName res is: ",res)
             pic_num+=1
 #        print("for polar angle ",polar_angle," crop finish ",pic_num," times")
     crop=0
@@ -365,5 +319,3 @@ print("\tJOB_DONE")
 for i in actor_dict:
     show_again=client.request('vset /object/'+str(i)+'/show')
 #    print('NOW visible actor: ',i,'\t',show_again)
-#show_Floor=client.request('vset /object/Floor/show')
-#show_Floor_14=client.request('vset /object/Floor_14/show')
