03d75ae2731b45a5957eab56b4ebfd70131214d3
atifkarim <atif0902082@gmail.com>
2019-08-27 14:14:57 +0200

    train image after augmntation

---
+++ classification_UE4_image/train_UE4_image_augmentation.py
@@ -0,0 +1,210 @@
+import numpy as np
+from skimage import io, color, exposure, transform
+from skimage.color import rgb2gray
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split  #it came from update scikit learn. https://stackoverflow.com/questions/40704484/importerror-no-module-named-model-selection
+import os
+import glob
+import h5py
+import keras
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, model_from_json
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D,Conv1D
+from keras.layers.pooling import MaxPooling2D
+
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras import backend as K
+K.set_image_data_format('channels_first')
+
+from matplotlib import pyplot as plt
+# get_ipython().run_line_magic('matplotlib', 'inline')
+from matplotlib.pylab import rcParams
+
+#import keras
+
+NUM_CLASSES = 2 # change it with respect to the desired class
+IMG_SIZE = 48 # change it if it desired
+IMG_depth = 3 # for RGB 3, for B&W it will be 1
+
+
+
+def preprocess_img(img):
+    # Histogram normalization in y
+    hsv = color.rgb2hsv(img)
+    hsv[:,:,2] = exposure.equalize_hist(hsv[:,:,2])
+    img = color.hsv2rgb(hsv)
+
+    # central scrop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0]//2, img.shape[1]//2
+    img = img[centre[0]-min_side//2:centre[0]+min_side//2,centre[1]-min_side//2:centre[1]+min_side//2,:]
+#    img = rgb2gray(img)
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    # roll color axis to axis 0
+    img = np.rollaxis(img,-1)
+
+    return img
+
+def get_class(img_path):
+    return int(img_path.split('/')[-2])
+#     return str(img_path.split('/')[-2]) # returning the folder name. If use -1 that means image name. consider the img_path.
+
+
+imgs = []
+labels = []
+root_dir = '/home/atif/machine_learning_stuff/ml_image/copy_image/'
+#path='/home/atif/training_by_several_learning_process/flower_photos/00000/'
+
+#all_img_paths = glob.glob(path+ '5547758_eea9edfd54_n_000.jpg')
+
+all_img_paths = glob.glob(os.path.join(root_dir, '*/*.png')) #I have done the training with .png format image. If another type of image will come
+                                                                                    #them .png will be changed by that extension
+np.random.shuffle(all_img_paths)
+for img_path in all_img_paths:
+    try:
+        img = preprocess_img(io.imread(img_path))
+        label = get_class(img_path)
+        imgs.append(img)
+        labels.append(label)
+
+        if len(imgs)%1200 == 0: print("Processed {}/{}".format(len(imgs), len(all_img_paths)))
+            #print("get it 2")
+    except (IOError, OSError):
+        print('missed', img_path)
+        pass
+
+
+
+
+
+X = np.array(imgs, dtype='float32') #Keeping the image as an array
+X = X.reshape(len(imgs),IMG_depth,IMG_SIZE,IMG_SIZE) # write (IMG_SIZE,IMG_SIZE,1 if you want channel last; 1= grayscale;3=RGB)
+# Y = np.eye(NUM_CLASSES, dtype='uint8')[labels]
+Y = keras.utils.to_categorical(labels, NUM_CLASSES)
+
+print('X shape: ', X.shape,' type: ',type(X))
+print('Y shape: ', Y.shape,' type: ',type(Y))
+
+
+
+
+def cnn_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=(3, IMG_SIZE, IMG_SIZE),
+                     activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(128, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+    return model
+
+model = cnn_model()
+
+lr = 0.01
+sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+          optimizer=sgd,
+          metrics=['accuracy'])
+
+
+
+
+
+path = '/home/atif/machine_learning_stuff/model_file_keras/'
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+datagen = ImageDataGenerator(featurewise_center=False,
+                            featurewise_std_normalization=False,
+                            width_shift_range=0.1,
+                            height_shift_range=0.1,
+                            zoom_range=0.2,
+                            shear_range=0.1,
+                            rotation_range=10.,)
+
+datagen.fit(X_train)
+
+model_augmentation = cnn_model()
+# let's train the model using SGD + momentum (how original).
+lr = 0.01
+sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+model_augmentation.compile(loss='categorical_crossentropy',
+          optimizer=sgd,
+          metrics=['accuracy'])
+
+
+def lr_schedule(epoch):
+    return lr*(0.1**int(epoch/10))
+
+
+nb_epoch = 1
+batch_size = 32
+do_train_model = model_augmentation.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size),
+                            steps_per_epoch=X_train.shape[0],
+                            epochs=nb_epoch,
+                            validation_data=(X_val, Y_val),
+                            callbacks=[LearningRateScheduler(lr_schedule),
+                                       ModelCheckpoint(path+'model_augmentation_1_epoch_27_aug.h5',save_best_only=True)]
+                           )
+
+
+
+
+# history = model.fit(X, Y, validation_split=0.33, epochs=1, batch_size=32, verbose=0)
+# list all data in history
+print(do_train_model.history.keys())
+# summarize history for accuracy
+plt.plot(do_train_model.history['acc'])
+plt.plot(do_train_model.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train_accuracy', 'validation_accuracy'], loc='upper left')
+plt.rcParams['figure.figsize'] =(20,10)
+plt.savefig(path+'augmentation_epoch_vs_accuracy.jpg')
+# plt.show()
+
+
+# In[38]:
+
+
+print(do_train_model.history.keys())
+# summarize history for accuracy
+plt.plot(do_train_model.history['loss'])
+plt.plot(do_train_model.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train_loss', 'validation_loss'], loc='upper left')
+plt.savefig(path+'augmentation_epoch_vs_loss.jpg')
+# plt.show()
