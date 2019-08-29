d345e4fd2da87fd0dc46630542a9b41b8d827500
atifkarim <atif0902082@gmail.com>
2019-08-29 12:08:33 +0200

    testing script added

---
+++ classification_UE4_image/test_classification.py
@@ -0,0 +1,89 @@
+import numpy as np
+from skimage import io, color, exposure, transform
+from skimage.color import rgb2gray
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split  #it came from update scikit learn. https://stackoverflow.com/questions/40704484/importerror-no-module-named-model-selection
+import os
+import glob
+import h5py
+import keras
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential, model_from_json
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D,Conv1D
+from keras.layers.pooling import MaxPooling2D
+
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras import backend as K
+K.set_image_data_format('channels_first')
+
+from matplotlib import pyplot as plt
+#get_ipython().run_line_magic('matplotlib', 'inline')
+from matplotlib.pylab import rcParams
+
+#import keras
+
+NUM_CLASSES = 16 # change it with respect to the desired class
+IMG_SIZE = 48 # change it if it desired
+IMG_depth = 3 # for RGB 3, for B&W it will be 1
+
+def preprocess_img(img):
+    # Histogram normalization in y
+    hsv = color.rgb2hsv(img)
+    hsv[:,:,2] = exposure.equalize_hist(hsv[:,:,2])
+    img = color.hsv2rgb(hsv)
+
+    # central scrop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0]//2, img.shape[1]//2
+    img = img[centre[0]-min_side//2:centre[0]+min_side//2,centre[1]-min_side//2:centre[1]+min_side//2,:]
+#    img = rgb2gray(img)
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    # roll color axis to axis 0
+    img = np.rollaxis(img,-1)
+
+    return img
+
+def get_class(img_path):
+    return int(img_path.split('/')[-2])
+#     return str(img_path.split('/')[-2]) # returning the folder name. If use -1 that means image name. consider the img_path.
+
+
+
+from keras.models import load_model
+model = load_model('/home/atif/machine_learning_stuff/model_file_keras/model_augmentation_1_epoch_27_aug.h5')
+
+import pandas as pd
+
+test = pd.read_csv('/home/atif/machine_learning_stuff/ml_image/test_image_keras_IAI.csv', sep=';')
+# test_image_path =  '/home/atif/machine_learning_stuff/ml_image/test_image_crop/'
+
+X_test = []
+y_test = []
+i = 0
+for file_name, class_id in zip(list(test['Filename']), list(test['ClassId'])):
+    print('filename: ', file_name, '\t classId: ', class_id)
+    # print('classId: ',class_id)
+    img_path = os.path.join('/home/atif/machine_learning_stuff/ml_image/test_image_26_aug/', file_name)
+    #     img_path = glob.glob(os.path.join(test_image_path, '*/*.jpg'))
+    X_test.append(preprocess_img(io.imread(img_path)))
+    y_test.append(class_id)
+
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+
+print(y_test)
+
+
+y_pred = model.predict_classes(X_test)
+acc = np.sum(y_pred==y_test)/np.size(y_pred)
+print("Test accuracy = {}".format(acc))
