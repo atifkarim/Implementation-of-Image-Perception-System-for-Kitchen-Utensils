6cafe9a0d5f522900e33df0927b5f4244869debd
atifkarim <atif0902082@gmail.com>
2019-09-20 01:23:36 +0200

    change in model will be done from local pc

--- classification_UE4_image/train_UE4_image_classification.py
+++ classification_UE4_image/train_UE4_image_classification.py
@@ -124,21 +124,28 @@ def cnn_model():
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
-
+    
     model.add(Conv2D(128, (3, 3), padding='same',
+                     input_shape=(3, IMG_SIZE, IMG_SIZE),
                      activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
-    model.add(Conv2D(128, (3, 3), padding='same',
+    model.add(Conv2D(256, (3, 3), padding='same',
                      activation='relu'))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(512, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1024, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(NUM_CLASSES, activation='softmax'))
     return model
