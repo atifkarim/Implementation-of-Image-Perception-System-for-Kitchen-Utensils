7e96b0bf01f443c50a3738c532fcab3d72845187
atifkarim <atif0902082@gmail.com>
2019-11-26 12:12:49 +0100

    merged testing folder of yolo from master

---
+++ testing_YOLO/yolo_testing_video.py
@@ -0,0 +1,174 @@
+import cv2 as cv
+import argparse
+import numpy as np
+import os
+import sys
+
+print(cv.__version__)
+
+# Initialize the parameters
+confThreshold = 0.1  #Confidence threshold
+nmsThreshold = 0.7   #Non-maximum suppression threshold#*
+inpWidth = 832       #Width of network's input image
+inpHeight = 832      #Height of network's input image
+
+def getOutputsNames(net):
+    # Get the names of all the layers in the network
+    layersNames = net.getLayerNames()
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+
+
+# Remove the bounding boxes with low confidence using non-maxima suppression
+def postprocess(frame, outs):
+#     global confThreshold
+#     global nmsThreshold
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+    classIds = []
+    confidences = []
+    boxes = []
+    # Scan through all the bounding boxes output from the network and keep only the
+    # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            print('val of confidence: ', confidence)
+            if confidence >= confThreshold:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences.
+    indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
+
+
+# Draw the predicted bounding box
+def drawPred(classId, conf, left, top, right, bottom):
+    # Draw a bounding box.
+    cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255))
+     
+    label = '%.2f' % conf
+         
+    # Get the label for the class name and its confidence
+    if classes:
+        assert(classId < len(classes))
+        label = '%s:%s' % (classes[classId], label)
+ 
+    #Display the label at the top of the bounding box
+    labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    top = max(top, labelSize[1])
+    cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+
+
+# Load names of classes
+classesFile = "/home/atif/darknet/data/obj.names"
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+# Give the configuration and weight files for the model and load the network using them.
+modelConfiguration = "/home/atif/darknet/cfg/yolo-obj.cfg"
+modelWeights = "/home/atif/darknet/backup/yolo-obj_15200.weights"
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+img = []
+vid= '/home/atif/darknet/data/test_video_output/viewmode_lit_delay_200_millisecond.avi'
+
+
+
+
+
+outputFile = "yolo_out_py.avi"
+if (img):
+    # Open the image file
+    if not os.path.isfile(img):
+        print("Input image file ", img, " doesn't exist")
+        sys.exit(1)
+    cap = cv.VideoCapture(img)
+    outputFile = img[:-4]+'_yolo_out_py.jpg'
+elif (vid):
+    # Open the video file
+    if not os.path.isfile(vid):
+        print("Input video file ", vid, " doesn't exist")
+        sys.exit(1)
+    cap = cv.VideoCapture(vid)
+    outputFile = vid[:-4]+'_yolo_out_py.avi'
+else:
+    # Webcam input
+    cap = cv.VideoCapture(0)
+
+# Get the video writer initialized to save the output video
+if (not img):
+
+    vid_writer = cv.VideoWriter(outputFile, cv.VideoWriter_fourcc('M','J','P','G'), 30, (round(cap.get(cv.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv.CAP_PROP_FRAME_HEIGHT))))
+
+
+
+while cv.waitKey(1) < 0:
+
+    # get frame from the video
+    hasFrame, frame = cap.read()
+
+    # Stop the program if reached end of video
+    if not hasFrame:
+        print("Done processing !!!")
+        print("Output file is stored as ", outputFile)
+        cv.waitKey(3000)
+        break
+
+    inpWidth = 2048
+    inpHeight= 1536
+    # Create a 4D blob from a frame.
+    blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+
+    # Sets the input to the network
+    net.setInput(blob)
+
+    # Runs the forward pass to get output of the output layers
+    outs = net.forward(getOutputsNames(net))
+
+    # Remove the bounding boxes with low confidence
+    postprocess(frame, outs)
+
+    # Put efficiency information. The function getPerfProfile returns the
+    # overall time for inference(t) and the timings for each of the layers(in layersTimes)
+    t, _ = net.getPerfProfile()
+    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+    cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
+    # Write the frame with the detection boxes
+    if (img):
+        cv.imwrite(outputFile, frame.astype(np.uint8));
+    else:
+        vid_writer.write(frame.astype(np.uint8))
+
+
+# Get the names of the output layers
+## checking from master AI"
