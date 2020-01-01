bc2935cc1cd753a0224e3e7f251dcf4710a045de
atifkarim <atif0902082@gmail.com>
2020-01-01 05:05:20 +0100

    obj.names file creation code merged

---
+++ testing_YOLO/creation_obj_names_file.py
@@ -0,0 +1,37 @@
+import os
+
+dirName = '/media/atif/BE0E05910E0543BD/University of Bremen MSc/problem_solving/convert_yolo_folder/Images/'
+
+current_dir = os.getcwd()
+print(current_dir)
+file_name = 'obj.names'
+
+obj_names_file_path = current_dir+'/'+file_name
+if not os.path.isfile(obj_names_file_path):
+    f = open(obj_names_file_path,'a')
+    f.close()
+    print('file now created')
+else:
+    os.remove(obj_names_file_path)
+    f = open(obj_names_file_path,'a')
+    f.close()
+    print('file removed and created')
+
+
+folder_list = []
+ent = os.listdir(dirName)
+for t in ent:
+    folder_list.append(t)
+
+print(folder_list)
+folder_list.sort()
+print(folder_list)
+
+
+f = open(obj_names_file_path, 'a')
+
+for x in folder_list:
+    print(x)
+    f.write(str(x))
+    f.write('\n')
+f.close()
